feda02aaadf75cd31b53951075b28fc53857b4a8
Piotr Chmielewski <chmiel.pio@gmail.com>
2012-01-22 19:44:15 +0100

    praca_domowa

---
+++ praca_domowa/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+   //int m, n;
+   //cout << "wprowadz 2 liczby" << endl;
+   //cin >> m >> n;
+   //cout << m/n << endl;
+   //cout << m%n << endl;
+
+
+   //int a;
+   //cout << "wprowadz liczbe" << endl;
+   //cin >> a;
+   //if (a>0) cout << "dodatnia" << endl;
+   //if (a<=0) cout << "niedodatnia" << endl;
+
+
+   //int a=0;
+   //int b=0;
+   //cout << "wprowadz liczbe" << endl;
+   //cin >> a;
+   //b=a%2;
+   //if (b==0) cout << "parzysta" << endl;
+   //else cout << "nieparzysta" << endl;
+
+
+   //int x;
+   //cout << "wprowadz liczbe" << endl;
+   //cin >> x;
+   //if (x>=10 and x<100) cout << "dwucyfrowa dodatnia" << endl;
+   //else cout << "inna" << endl;
+
+
+   //char a;
+   //cout << "wprowadz znak" << endl;
+   //cin >> a;
+   //if (a>47 and a<58) cout << "cyfra";
+   //if (a>96 and a<123) cout << "mala litera";
+   //if (a>64 and a<91) cout << "wielka litera";
+
+
+   //double x;
+   //double z;
+   //cout << "wprowadz liczbe" << endl;
+   //cin >> x;
+   //if (x==0) cout << "dzielenie przez 0 jest niewykonalne" << endl;
+   //else
+   //z=1/x;
+   //cout << z << endl;
+
+
+
+   double x=0;
+   cout << "wprowadz liczbe" << endl;
+   cin >> x;
+   if (x<0) cout << "0";
+   else cout << (sqrt(x) + x)/(2 + x);
+
+
+}
+
+
